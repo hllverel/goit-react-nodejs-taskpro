76a8e5ccfddef2031d7b05e1ed68a1ea2b0161b1
Yasin Kara <theyasin@icloud.com>
2026-07-21 21:15:47 +0300

    feat: add board and column workspace

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,19 @@
 import Header from "../components/Header/Header.jsx";
 import Sidebar from "../components/Sidebar/Sidebar.jsx";
-import ScreensPage from "../components/ScreensPage/ScreensPage.jsx";
+import MainDashboard from "../components/MainDashboard/MainDashboard.jsx";
+import { BoardWorkspaceProvider } from "../components/BoardWorkspace/BoardWorkspaceContext.jsx";
 
 function HamePage() {
     return (
-      <>
-        <h2>HomePage</h2>
-        <Header />
-        <Sidebar />
-        <ScreensPage />
-      </>
+      <BoardWorkspaceProvider>
+        <div className="taskpro-layout">
+          <Sidebar />
+          <div className="taskpro-content">
+            <Header />
+            <MainDashboard />
+          </div>
+        </div>
+      </BoardWorkspaceProvider>
   );
 }
 
