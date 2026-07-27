a51121d2b173d9bd2e259a32a0b262f977c8aa35
hllverel <hllozdemir@live.com>
2026-07-27 17:46:13 +0300

    Configure frontend for production API

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,12 @@ export default function setupServer() {
   const PORT = process.env.PORT || 3000;
 
   app.use(express.json({ limit: '10mb' }));
-  app.use(cors());
+  app.use(
+    cors({
+      origin: process.env.FRONTEND_URL,
+      credentials: true,
+    })
+  );
   app.use(cookieParser());
 
   app.use(pino({
